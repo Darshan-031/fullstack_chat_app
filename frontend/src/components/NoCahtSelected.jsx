1e535ee9dc934d6ba30c194fe8eaad73a5649e50
031_darshan <142666490+Darshan-031@users.noreply.github.com>
2025-03-04 09:55:28 +0530

    Update NoCahtSelected.jsx
    name changed from Chatty -> chatbox

--- frontend/src/components/NoCahtSelected.jsx
+++ frontend/src/components/NoCahtSelected.jsx
@@ -13,7 +13,7 @@ const NoCahtSelected = () => {
                 </div>
             </div>
             {/* Welcome text  */}
-            <h2 className="text-2xl font-bold">Welcome to Chatty</h2>
+            <h2 className="text-2xl font-bold">Welcome to ChatBox</h2>
             <p className="text-base-content/60">Select a conversation from the sidebar to start chatting</p>
         </div>
     </div>
